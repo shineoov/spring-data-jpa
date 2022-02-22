0a656607af41a37e8afc3b6c9066386a91bbe42f
shineoov <shineoov88@gmail.com>
2022-02-22 14:18:21 +0900

    복합 키 - @EmbeddedId

---
+++ src/main/java/shineoov/springdatajpa/domain/composite_key/embedded/Parent.java
@@ -0,0 +1,15 @@
+package shineoov.springdatajpa.domain.composite_key.embedded;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity(name = "ParentEmbedded")
+@Table(name = "parent_embedded")
+public class Parent {
+
+    @EmbeddedId
+    private ParentId id;
+
+    private String name;
+}
---
+++ src/main/java/shineoov/springdatajpa/domain/composite_key/embedded/ParentId.java
@@ -0,0 +1,27 @@
+package shineoov.springdatajpa.domain.composite_key.embedded;
+
+import javax.persistence.Column;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class ParentId implements Serializable {
+
+    @Column(name = "parent_id1")
+    private String id1;
+
+    @Column(name = "parent_id2")
+    private String id2;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ParentId parentId = (ParentId) o;
+        return Objects.equals(id1, parentId.id1) && Objects.equals(id2, parentId.id2);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id1, id2);
+    }
+}
