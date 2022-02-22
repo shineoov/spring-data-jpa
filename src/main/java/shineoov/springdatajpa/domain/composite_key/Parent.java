34c1f76c5fab2e40a123a3982aac05a9e342236b
shineoov <shineoov88@gmail.com>
2022-02-22 12:22:20 +0900

    복합 키 - @IdClass

---
+++ src/main/java/shineoov/springdatajpa/domain/composite_key/Parent.java
@@ -0,0 +1,27 @@
+package shineoov.springdatajpa.domain.composite_key;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+
+@Setter
+@Getter
+@Entity
+@IdClass(ParentId.class)
+public class Parent {
+
+    @Id
+    @Column(name = "parent_id1")
+    private String id1;
+
+    @Id
+    @Column(name = "parent_id2")
+    private String id2;
+
+    private String name;
+
+}
---
+++ src/test/java/shineoov/springdatajpa/basic/CompositeKeyTest.java
@@ -0,0 +1,52 @@
+package shineoov.springdatajpa.basic;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import shineoov.springdatajpa.domain.composite_key.Parent;
+import shineoov.springdatajpa.domain.composite_key.ParentId;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+@Transactional
+@Rollback(value = false)
+@SpringBootTest
+public class CompositeKeyTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    @DisplayName("복합키 저장 및 조회")
+    void save_and_find() {
+        //given
+        String firstId = "ID1";
+        String secondId = "ID2";
+
+        Parent parent = new Parent();
+        parent.setId1(firstId);
+        parent.setId2(secondId);
+        parent.setName("Oh");
+
+        em.persist(parent);
+        em.flush();
+        em.clear();
+
+        //when
+        ParentId parentId = new ParentId(firstId, secondId);
+        Parent findParent = em.find(Parent.class, parentId);
+
+        //then
+        assertAll(
+                () -> Assertions.assertThat(findParent.getId1()).isEqualTo(firstId),
+                () -> Assertions.assertThat(findParent.getId2()).isEqualTo(secondId)
+        );
+    }
+}
