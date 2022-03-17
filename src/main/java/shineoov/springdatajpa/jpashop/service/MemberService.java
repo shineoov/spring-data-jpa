7a7c06c804792ee0022ae086eaf1525f5d94e9a5
shineoov <shineoov88@gmail.com>
2022-03-17 21:46:38 +0900

    JpaShop 예제 - 멤버 서비스, 리포지토리

---
+++ src/main/java/shineoov/springdatajpa/jpashop/service/MemberService.java
@@ -0,0 +1,41 @@
+package shineoov.springdatajpa.jpashop.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import shineoov.springdatajpa.jpashop.domain.Member;
+import shineoov.springdatajpa.jpashop.repository.MemberRepository;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+@Transactional
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public Long join(Member member) {
+
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
